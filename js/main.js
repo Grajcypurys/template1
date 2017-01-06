6ffaafaa8c473163fef099a03cb6712f45050565
Grajcypurys <wisniewskimaciej7@gmail.com>
2017-01-06 11:26:56 +0100

    Add slider and parallax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log("js workin!")
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 800,
+    autoplay: 6000,
+    a11y: true,
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide',
+    keyboardControl: true,
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+  });
+
+( function () {
+    var btn = document.getElementById("btn-menu")
+    var menu = document.querySelector(".page-nav")
+    btn.addEventListener("click", function(){
+        if (menu.classList) {
+            menu.classList.toggle("page-nav__closed");
+        }
+        else {
+            var classes = menu.className.split(' ');
+            var existingIndex = classes.indexOf("page-nav__closed");
+
+            if (existingIndex >= 0)
+                classes.splice(existingIndex, 1);
+            else
+                classes.push("page-nav__closed");
+                menu.className = classes.join(' ');
+        }
+        console.log(btn)
+        console.log(menu)
+    }, false)
+} () );
