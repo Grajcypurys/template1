db711987a75d359a6662f41fd49ddea901ca9e58
Grajcypurys <wisniewskimaciej7@gmail.com>
2017-01-09 14:31:01 +0100

    Add menu button function with callbacks for svg and classList

--- js/main.js
+++ js/main.js
@@ -23,48 +23,38 @@ var mySwiper = new Swiper ('.swiper-container', {
     var itemsClass = ["page-nav__opened", "hamburger-svg__ring-filled", "hamburger-opened"]
     btn.addEventListener("click", function(){
         
+        if (ham.className === "hamburger") {
+                btn.setAttribute("title", "Close menu")
+            }
+        else {
+                btn.setAttribute("title", "Open menu")
+            }
+        
         if (menu.classList) {
             for (var i = 0; i<items.length; i++){
                 items[i].classList.toggle(itemsClass[i])
             }
-            // menu.classList.toggle("page-nav__opened");
-            // ring.classList.toggle("hamburger-svg__ring-filled");
-            // ham.classList.toggle("hamburger-opened");
         }
         else {
-            var classes = menu.className.split(' ');
-            var existingIndex = classes.indexOf("page-nav__opened");
-            // var classesRing = ring.className.split(' ');
-            // var existingIndexRing = classesRing.indexOf("hamburger-svg__ring-filled");
-            // var classesHam = ham.className.split(' ');
-            // var existingIndexHam = classesHam.indexOf("hamburger-opened");
-            
-            if (existingIndex >= 0) {
-                    var x = "";
-                    var classTab = "";
-                    var index = "";
-//                    classes.splice(existingIndex, 1);
-//                    classesRing.splice(existingIndexRing, 1);
-                    for (var i = 0; i < items.length; i++){
-                        if(items[i].parentNode.nodeName === "svg"){
-                            x = items[i].className.baseVal + '';
-                            classTab = x.split(' ');
-                            index = classTab.indexOf(itemsClass[i])
-                            classTab.splice(index, 1)
-                        }else{
-                            x = items[i].className + '';
-                            classTab = x.split(' ');
-                            index = classTab.indexOf(itemsClass[i])
-                            classTab.splice(index, 1)
+            ( function () {
+                for (var i = 0, ilen = items.length; i < ilen; i++) {
+                    var reg = new RegExp('(\\s|^)' + itemsClass[i] + '(\\s|$)')
+                    if (items[i].parentNode.nodeName !== "svg") {
+                        if (ham.className === "hamburger") {
+                            items[i].className += " " + itemsClass[i]
+                        } else {
+                            items[i].className = items[i].className.replace(reg, '')
+                        }
+                    } else {
+                        if (ham.className === "hamburger") {
+                            items[i].className.baseVal += " " + itemsClass[i]
+                        } else {
+                            items[i].className.baseVal = items[i].className.baseVal.replace(reg, '')
                         }
                     }
-            }
-            else
-                
-                classes.push("page-nav__opened");
-                classesRing.push("hamburger-svg__ring-filled");
-                menu.className = classes.join(' ');
-                ring.className = classesRing.join(' ');
+                }
+            } () );
         }
+        
     }, false)
 } () );
