8af7fd98f6f93e49b64120fbfda27101139359c1
Grajcypurys <wisniewskimaciej7@gmail.com>
2017-01-11 16:27:25 +0100

    Add function hiding bar when menu is opened

--- js/main.js
+++ js/main.js
@@ -18,70 +18,75 @@ var mySwiper = new Swiper ('.swiper-container', {
     var btn = document.getElementById("btn-menu")
     var menu = document.querySelector(".page-nav")
     var ring = document.querySelector(".hamburger-svg__ring")
-    var ham = document.querySelector(".hamburger")
+    var header = document.getElementById('page-header')
+    var ham = document.getElementById("hamburger")
+    var logo = document.getElementById("page-logo")
     var items = [menu, ring, ham]
     var itemsClass = ["page-nav__opened", "hamburger-svg__ring-filled", "hamburger-opened"]
-
-    btn.addEventListener("click", function(){
+    
+    function classToggle(checker, checkerClass, arr, arrClass){
         
-        if (ham.className === "hamburger") {
-                btn.setAttribute("title", "Close menu")
-            }
-        else {
-                btn.setAttribute("title", "Open menu")
-            }
-        
-        if (menu.classList) {
-            for (var i = 0; i<items.length; i++){
-                items[i].classList.toggle(itemsClass[i])
+        if (checker.classList) {
+            for (var i = 0, ilen = arr.length; i < ilen; i++){
+                arr[i].classList.toggle(arrClass[i])
             }
         }
         else {
             ( function () {
-                for (var i = 0, ilen = items.length; i < ilen; i++) {
-                    var reg = new RegExp('(\\s|^)' + itemsClass[i] + '(\\s|$)')
-                    if (items[i].parentNode.nodeName !== "svg") {
-                        if (ham.className === "hamburger") {
-                            items[i].className += " " + itemsClass[i]
+                for (var i = 0, ilen = arr.length; i < ilen; i++) {
+                    var reg = new RegExp('(\\s|^)' + arrClass[i] + '(\\s|$)')
+                    if (arr[i].parentNode.nodeName !== "svg") {
+                        if (checker.className === '"'+checkerClass+'"') {
+                            arr[i].className += " " + arrClass[i]
                         } else {
-                            items[i].className = items[i].className.replace(reg, '')
+                            arr[i].className = arr[i].className.replace(reg, '')
                         }
                     } else {
-                        if (ham.className === "hamburger") {
-                            items[i].className.baseVal += " " + itemsClass[i]
+                        if (checker.className === '"'+checkerClass+'"') {
+                            arr[i].className.baseVal += " " + arrClass[i]
                         } else {
-                            items[i].className.baseVal = items[i].className.baseVal.replace(reg, '')
+                            arr[i].className.baseVal = arr[i].className.baseVal.replace(reg, '')
                         }
                     }
                 }
             } () );
         }
         
+    }
+    
+    function sticky () {
+        
+        if ( document.body.scrollTop + document.documentElement.scrollTop > 0 ) {
+            header.classList.add("page-header__sticky")
+            ham.classList.add("hamburger__sticky")
+            logo.setAttribute("src", "img/logo-dark.png")
+        }
+        else {
+            header.classList.remove("page-header__sticky")
+            ham.classList.remove("hamburger__sticky")
+            logo.setAttribute("src", "img/logo.png")
+        }
+        
+    }
+    
+    btn.addEventListener("click", function () {
+        
+        if (ham.className === "hamburger" || ham.className === "hamburger hamburger__sticky") {
+            btn.setAttribute("title", "Close menu")
+            header.classList.remove("page-header__sticky")
+            ham.classList.remove("hamburger__sticky")
+            logo.setAttribute("src", "img/logo.png")
+            window.removeEventListener("scroll", sticky, false)
+        }
+        else {
+            btn.setAttribute("title", "Open menu")
+            window.addEventListener("scroll", sticky, false)
+        }
+        
+        classToggle(ham, hamburger, items, itemsClass)
+        
     }, false)
+    
+    window.addEventListener("scroll", sticky, false)
+    
 } () );
-
-//(function(){
-//   var stickyNavTop = $('.page-header').offset().top;
-// 
-//   var stickyNav = function(){
-//   var scrollTop = $(window).scrollTop();
-// 
-//   if (scrollTop > stickyNavTop) { 
-//      $('.page-header').addClass('page-header__sticky');
-//   } else {
-//      $('.page-header').removeClass('page-header__sticky');
-//    }
-//   };
-// 
-// 
-//   $(window).scroll(function() {
-//      stickyNav();
-//   });
-//}());
-
-window.onscroll = function() {
-    var sticky = window.document.getElementById('page-header');
-    if( document.body.scrollTop + document.documentElement.scrollTop > 0)
-    {sticky.classList.add("page-header__sticky")}
-    else sticky.classList.remove("page-header__sticky");
-};
